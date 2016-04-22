47e841de56fdfb18711d734beb77f49cc1db4326
Justin Lawen <Justin@LesserHydra.com>
2016-04-22 15:45:57 -0500

    Flesh out config file

--- src/main/java/com/lesserhydra/secondchance/ConfigOptions.java
+++ src/main/java/com/lesserhydra/secondchance/ConfigOptions.java
@@ -36,27 +36,23 @@ class ConfigOptions {
 	public final boolean isProtected;
 	
 	public ConfigOptions(FileConfiguration config) {
-		this.holdItems = config.getBoolean("holdItems", true);
+		this.holdItems = config.getBoolean("Hold Items", true);
+		this.holdExp = config.getBoolean("Hold Experience", true);
+		this.dropItemsOnForget = config.getBoolean("Drop Items When Forgotten", true);
+		this.dropExpOnForget = config.getBoolean("Drop Experience When Forgotten", false);
+		this.maxPerPlayer = config.getInt("Player Deathpoint Maximum", 1);
+		this.isProtected = config.getBoolean("Owner Based", true);;
+		this.particleDelay = config.getLong("Particle Timer Delay", 20);
 		
-		this.holdExp = config.getBoolean("holdExp", true);
-		this.dropItemsOnForget = config.getBoolean("dropItemsOnForget", true);
-		this.dropExpOnForget = config.getBoolean("dropExpOnForget", false);
+		this.particlePrimary = Particle.valueOf(config.getString("Primary Particles.Type", "PORTAL"));
+		this.particlePrimaryCount = config.getInt("Primary Particles.Count", 50);
+		this.particlePrimarySpread = config.getDouble("Primary Particles.Spread", 0.2);
+		this.particlePrimarySpeed = config.getDouble("Primary Particles.Speed", 0.5);
 		
-		this.maxPerPlayer = config.getInt("maxPerPlayer", 1);
-		
-		this.particleDelay = config.getLong("particleDelay", 20);
-		
-		this.particlePrimary = Particle.valueOf(config.getString("particlePrimary", "PORTAL"));
-		this.particlePrimaryCount = config.getInt("particlePrimaryCount", 50);
-		this.particlePrimarySpread = config.getDouble("particlePrimarySpread", 0.2);
-		this.particlePrimarySpeed = config.getDouble("particlePrimarySpeed", 0.5);
-		
-		this.particleSecondary = Particle.valueOf(config.getString("particleSecondary", "END_ROD"));
-		this.particleSecondaryCount = config.getInt("particleSecondaryCount", 15);
-		this.particleSecondarySpread = config.getDouble("particleSecondarySpread", 10);
-		this.particleSecondarySpeed = config.getDouble("particleSecondarySpeed", 0.1);
-		
-		this.isProtected = config.getBoolean("isProtected", true);;
+		this.particleSecondary = Particle.valueOf(config.getString("Secondary Particles.Type", "END_ROD"));
+		this.particleSecondaryCount = config.getInt("Secondary Particles.Count", 15);
+		this.particleSecondarySpread = config.getDouble("Secondary Particles.Spread", 10);
+		this.particleSecondarySpeed = config.getDouble("Secondary Particles.Speed", 0.1);
 	}
 	
 }
