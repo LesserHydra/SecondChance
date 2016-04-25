1ac9770354c604a491560b00ea7b2861fbf0959b
Justin Lawen <Justin@LesserHydra.com>
2016-04-25 17:14:54 -0500

    Fix purposeful bug left in from debugging
    *Facepalm*

--- src/test/java/com/lesserhydra/bukkitutil/ExpUtilTest.java
+++ src/test/java/com/lesserhydra/bukkitutil/ExpUtilTest.java
@@ -9,7 +9,7 @@ public class ExpUtilTest {
 	//Small test to check accuracy of calculateXpFromLevel()
 	@Test public void experienceCalculation() {
 		for (int i = 0; i <= 1000; i++) {
-			int formula = ExpUtil.calculateXpFromLevel(i+1);
+			int formula = ExpUtil.calculateXpFromLevel(i);
 			int iteration = IntStream.range(0, i)
 					.map(ExpUtil::calculateXpForNextLevel)
 					.sum();
