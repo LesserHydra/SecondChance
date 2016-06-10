f9e2455433ce265aeab651c2af3831bf93063e00
Justin Lawen <Justin@LesserHydra.com>
2016-06-09 22:28:09 -0500

    Make list subcommand remember listed points for five minutes after command, and add color

--- src/main/java/com/lesserhydra/secondchance/command/ListedPoints.java
+++ src/main/java/com/lesserhydra/secondchance/command/ListedPoints.java
@@ -3,12 +3,10 @@ package com.lesserhydra.secondchance.command;
 import com.lesserhydra.secondchance.Deathpoint;
 import com.lesserhydra.secondchance.SecondChance;
 import com.lesserhydra.secondchance.WorldHandler;
-import org.bukkit.Bukkit;
-import org.bukkit.Location;
-import org.bukkit.OfflinePlayer;
-import org.bukkit.World;
+import org.bukkit.*;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitTask;
 
 import javax.annotation.Nullable;
 import java.util.*;
@@ -16,20 +14,21 @@ import java.util.stream.Collectors;
 
 class ListedPoints {
 	
-	//TODO: Remove after period of time
-    private final Map<String, ArrayList<Deathpoint>> playerLists = new HashMap<>();
+	private static final long RESET_DELAY = 6000; //Five minutes
+	private final Map<String, ArrayList<Deathpoint>> playerLists = new HashMap<>();
+	private final Map<String, BukkitTask> resetTasks = new HashMap<>();
 	
 	
 	public void list(CommandSender sender, @Nullable OfflinePlayer owner, @Nullable World world, int radius) {
 		if (radius >= 0) {
 			if (!(sender instanceof Player)) {
-				sender.sendMessage("Can only check for distance if run by a player.");
+				sender.sendMessage(ChatColor.RED + "Error: Can only check for distance if run by a player.");
 				return;
 			}
 			
 			Player playerSender = (Player) sender;
 			if (world != null && !playerSender.getWorld().equals(world)) {
-				sender.sendMessage("Cannot check for distance in another world.");
+				sender.sendMessage(ChatColor.RED + "Error: Cannot check for distance in another world.");
 				return;
 			}
 			
@@ -41,7 +40,7 @@ class ListedPoints {
 	}
 	
 	private void list(CommandSender sender, @Nullable World world, @Nullable OfflinePlayer owner) {
-		String message = "Showing deathpoints";
+		String message = ChatColor.BLUE + "Showing deathpoints";
 		if (owner != null) message += " for " + owner.getName();
 		if (world != null) message += " in " + world.getName();
 		message += ":";
@@ -57,7 +56,7 @@ class ListedPoints {
 	}
 	
 	private void list(Player sender, OfflinePlayer owner, int radius) {
-		String message = "Showing deathpoints";
+		String message = ChatColor.BLUE + "Showing deathpoints";
 		if (owner != null) message += " for " + owner.getName();
 		message += " within " + radius + "m:";
 		sender.sendMessage(message);
@@ -73,24 +72,39 @@ class ListedPoints {
 		showListToPlayer(sender, matching);
 	}
 	
+	private void showListToPlayer(CommandSender sender, ArrayList<Deathpoint> list) {
+		//(Re)Start reset timer
+		BukkitTask resetTask = resetTasks.get(sender.getName());
+		if (resetTask != null) resetTask.cancel();
+		resetTask = Bukkit.getScheduler().runTaskLater(SecondChance.instance(), () -> {
+			resetTasks.remove(sender.getName());
+			playerLists.remove(sender.getName());
+		}, RESET_DELAY);
+		resetTasks.put(sender.getName(), resetTask);
+		
+		//Remember indices
+		playerLists.put(sender.getName(), list);
+		
+		//Show to player
+		for (int i = 0; i < list.size(); i++) {
+			Deathpoint point = list.get(i);
+			Location loc = point.getLocation();
+			OfflinePlayer owner = Bukkit.getOfflinePlayer(point.getOwnerUniqueId());
+			sender.sendMessage(ChatColor.AQUA.toString() + i + ": " + owner.getName() + " @" + loc.getWorld().getName()
+					+ " (" + loc.getBlockX() + ", " + loc.getBlockY() + ", " + loc.getBlockZ() + ")");
+		}
+		
+		//Send reset info message
+		if (list.isEmpty()) sender.sendMessage(ChatColor.RED + "None found.");
+		else sender.sendMessage(ChatColor.GREEN + "Indices are valid for the next " + RESET_DELAY/1200 + " minutes.");
+	}
+	
 	@Nullable
-	public Deathpoint getDeathpoint(CommandSender sender, int index) {
+	Deathpoint getDeathpoint(CommandSender sender, int index) {
 		ArrayList<Deathpoint> list = playerLists.get(sender.getName());
 		if (list == null) return null;
 		if (index < 0 || index >= list.size()) return null;
 		return list.get(index);
 	}
-	
-	private void showListToPlayer(CommandSender sender, ArrayList<Deathpoint> list) {
-		playerLists.put(sender.getName(), list);
-		for (int i = 0; i < list.size(); i++) {
-			Deathpoint point = list.get(i);
-			Location loc = point.getLocation();
-			OfflinePlayer owner = Bukkit.getOfflinePlayer(point.getOwnerUniqueId());
-			sender.sendMessage(i + ": " + owner.getName() + " @" + loc.getWorld().getName() + "(" + loc.getBlockX()
-					+ ", " + loc.getBlockY() + ", " + loc.getBlockZ() + ")");
-		}
-	}
-	
 	
 }
